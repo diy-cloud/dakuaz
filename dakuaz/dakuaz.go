973b0139f5f528d7826de1ece105901d70b861b4
snowmerak <snowmerak@outlook.com>
2022-01-12 21:45:23 +0900

    Update dakuaz.go

--- dakuaz/dakuaz.go
+++ dakuaz/dakuaz.go
@@ -11,22 +11,23 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
-const tokenSize = 64
-const idSize = 32
+const TokenSize = 64
+const IdSize = 32
 const classSize = 1
 const levelSize = 1
 const expireTimeSize = 8
-const hashSize = 32
-const signatureSize = 114
+const HashSize = 32
+const SignatureSize = 114
+const DakuazSize = TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize
 
 type Dakuaz struct {
-	Token     [tokenSize]byte
-	Id        [idSize]byte
+	Token     [TokenSize]byte
+	Id        [IdSize]byte
 	Class     int8
 	Level     int8
 	ExpireAt  int64
-	Hash      [hashSize]byte
-	Signature [signatureSize]byte
+	Hash      [HashSize]byte
+	Signature [SignatureSize]byte
 }
 
 func New(id string, class int8, level int8, duration time.Duration) *Dakuaz {
@@ -70,27 +71,27 @@ func (d *Dakuaz) MakeSignature(seed [57]byte) error {
 	return nil
 }
 
-func (d *Dakuaz) Serialize() [tokenSize + idSize + classSize + levelSize + expireTimeSize + hashSize + signatureSize]byte {
-	buf := [tokenSize + idSize + classSize + levelSize + expireTimeSize + hashSize + signatureSize]byte{}
-	copy(buf[:tokenSize], d.Token[:])
-	copy(buf[tokenSize:tokenSize+idSize], d.Id[:])
-	buf[tokenSize+idSize] = byte(d.Class)
-	buf[tokenSize+idSize+classSize] = byte(d.Level)
-	binary.BigEndian.PutUint64(buf[tokenSize+idSize+classSize+levelSize:], uint64(d.ExpireAt))
-	copy(buf[tokenSize+idSize+classSize+levelSize+expireTimeSize:tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize], d.Hash[:])
-	copy(buf[tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize:tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize+signatureSize], d.Signature[:])
+func (d *Dakuaz) Serialize() [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte {
+	buf := [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte{}
+	copy(buf[:TokenSize], d.Token[:])
+	copy(buf[TokenSize:TokenSize+IdSize], d.Id[:])
+	buf[TokenSize+IdSize] = byte(d.Class)
+	buf[TokenSize+IdSize+classSize] = byte(d.Level)
+	binary.BigEndian.PutUint64(buf[TokenSize+IdSize+classSize+levelSize:], uint64(d.ExpireAt))
+	copy(buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize], d.Hash[:])
+	copy(buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize+SignatureSize], d.Signature[:])
 	return buf
 }
 
-func Deserialize(buf [tokenSize + idSize + classSize + levelSize + expireTimeSize + hashSize + signatureSize]byte) *Dakuaz {
+func Deserialize(buf [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte) *Dakuaz {
 	d := &Dakuaz{}
-	copy(d.Token[:], buf[:tokenSize])
-	copy(d.Id[:], buf[tokenSize:tokenSize+idSize])
-	d.Class = int8(buf[tokenSize+idSize])
-	d.Level = int8(buf[tokenSize+idSize+classSize])
-	d.ExpireAt = int64(binary.BigEndian.Uint64(buf[tokenSize+idSize+classSize+levelSize : tokenSize+idSize+classSize+levelSize+expireTimeSize]))
-	copy(d.Hash[:], buf[tokenSize+idSize+classSize+levelSize+expireTimeSize:tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize])
-	copy(d.Signature[:], buf[tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize:tokenSize+idSize+classSize+levelSize+expireTimeSize+hashSize+signatureSize])
+	copy(d.Token[:], buf[:TokenSize])
+	copy(d.Id[:], buf[TokenSize:TokenSize+IdSize])
+	d.Class = int8(buf[TokenSize+IdSize])
+	d.Level = int8(buf[TokenSize+IdSize+classSize])
+	d.ExpireAt = int64(binary.BigEndian.Uint64(buf[TokenSize+IdSize+classSize+levelSize : TokenSize+IdSize+classSize+levelSize+expireTimeSize]))
+	copy(d.Hash[:], buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize])
+	copy(d.Signature[:], buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize+SignatureSize])
 	return d
 }
 
