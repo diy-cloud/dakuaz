2256181a4e131e5104e425cb78645a5ec9d10420
snowmerak <snowmerak@outlook.com>
2022-01-31 02:02:46 +0900

    restruct dakuaz

--- dakuaz.go
+++ dakuaz.go
@@ -16,9 +16,9 @@ const IdSize = 32
 const classSize = 1
 const levelSize = 1
 const expireTimeSize = 8
-const HashSize = 32
+const boolSize = 1
 const SignatureSize = 114
-const DakuazSize = TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize
+const DakuazSize = TokenSize + IdSize + classSize + levelSize + expireTimeSize + boolSize + SignatureSize
 
 type Dakuaz struct {
 	Token     [TokenSize]byte
@@ -26,16 +26,19 @@ type Dakuaz struct {
 	Class     int8
 	Level     int8
 	ExpireAt  int64
-	Hash      [HashSize]byte
+	Echo      bool
 	Signature [SignatureSize]byte
+
+	hash [32]byte
 }
 
-func New(id string, class int8, level int8, duration time.Duration) *Dakuaz {
+func New(id string, class int8, level int8, duration time.Duration, echo bool) *Dakuaz {
 	d := &Dakuaz{}
 	d.Id = blake2b.Sum256([]byte(id))
 	d.Class = class
 	d.Level = level
 	d.ExpireAt = time.Now().Add(duration).Unix()
+	d.Echo = echo
 	return d
 }
 
@@ -57,13 +60,13 @@ func (d *Dakuaz) MakeHash() error {
 	hash.Write(buf[:])
 	binary.BigEndian.PutUint64(buf[:], uint64(d.ExpireAt))
 	hash.Write(buf[:])
-	d.Hash = blake2b.Sum256(hash.Sum(nil))
+	d.hash = blake2b.Sum256(hash.Sum(nil))
 	return nil
 }
 
 func (d *Dakuaz) MakeSignature(seed [57]byte) error {
 	priv := ed448.NewKeyFromSeed(seed[:])
-	sign, err := priv.Sign(rand.Reader, d.Hash[:], crypto.Hash(0))
+	sign, err := priv.Sign(rand.Reader, d.hash[:], crypto.Hash(0))
 	if err != nil {
 		return fmt.Errorf("dakuaz.MakeSignature: %w", err)
 	}
@@ -71,40 +74,46 @@ func (d *Dakuaz) MakeSignature(seed [57]byte) error {
 	return nil
 }
 
-func (d *Dakuaz) Serialize() [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte {
-	buf := [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte{}
+func (d *Dakuaz) Serialize() [TokenSize + IdSize + classSize + levelSize + expireTimeSize + boolSize + SignatureSize]byte {
+	buf := [TokenSize + IdSize + classSize + levelSize + expireTimeSize + boolSize + SignatureSize]byte{}
 	copy(buf[:TokenSize], d.Token[:])
 	copy(buf[TokenSize:TokenSize+IdSize], d.Id[:])
 	buf[TokenSize+IdSize] = byte(d.Class)
 	buf[TokenSize+IdSize+classSize] = byte(d.Level)
 	binary.BigEndian.PutUint64(buf[TokenSize+IdSize+classSize+levelSize:], uint64(d.ExpireAt))
-	copy(buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize], d.Hash[:])
-	copy(buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize+SignatureSize], d.Signature[:])
+	buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize] = byte(0)
+	if d.Echo {
+		buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize] = byte(1)
+	}
+	copy(buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+boolSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+boolSize+SignatureSize], d.Signature[:])
 	return buf
 }
 
-func Deserialize(buf [TokenSize + IdSize + classSize + levelSize + expireTimeSize + HashSize + SignatureSize]byte) *Dakuaz {
+func Deserialize(buf [TokenSize + IdSize + classSize + levelSize + expireTimeSize + boolSize + SignatureSize]byte) *Dakuaz {
 	d := &Dakuaz{}
 	copy(d.Token[:], buf[:TokenSize])
 	copy(d.Id[:], buf[TokenSize:TokenSize+IdSize])
 	d.Class = int8(buf[TokenSize+IdSize])
 	d.Level = int8(buf[TokenSize+IdSize+classSize])
 	d.ExpireAt = int64(binary.BigEndian.Uint64(buf[TokenSize+IdSize+classSize+levelSize : TokenSize+IdSize+classSize+levelSize+expireTimeSize]))
-	copy(d.Hash[:], buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize])
-	copy(d.Signature[:], buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+HashSize+SignatureSize])
+	d.Echo = false
+	if buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize] == byte(1) {
+		d.Echo = true
+	}
+	copy(d.Signature[:], buf[TokenSize+IdSize+classSize+levelSize+expireTimeSize+boolSize:TokenSize+IdSize+classSize+levelSize+expireTimeSize+boolSize+SignatureSize])
 	return d
 }
 
 func (d *Dakuaz) Verify(key [57]byte) bool {
 	priv := ed448.NewKeyFromSeed(key[:])
-	hash := d.Hash
+	hash := d.Echo
 	if err := d.MakeHash(); err != nil {
 		return false
 	}
-	if hash != d.Hash {
+	if hash != d.Echo {
 		return false
 	}
-	return ed448.Verify(priv.Public().(ed448.PublicKey), d.Hash[:], d.Signature[:], "")
+	return ed448.Verify(priv.Public().(ed448.PublicKey), d.hash[:], d.Signature[:], "")
 }
 
 func (d *Dakuaz) IsExpired() bool {
